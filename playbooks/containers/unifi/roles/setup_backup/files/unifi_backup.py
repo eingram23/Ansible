91774dd634d2028251cd4d687bf349e0144a71d2
Edward Ingram <edward.ingram23@gmail.com>
2022-05-22 15:43:26 -0700

    changed base user

--- playbooks/containers/unifi/roles/setup_backup/files/unifi_backup.py
+++ playbooks/containers/unifi/roles/setup_backup/files/unifi_backup.py
@@ -3,6 +3,6 @@
 import os
 
 os.system('podman pod stop unifi')
-os.system('podman run --volumes-from unifi-controller --privileged --rm -v $(pwd):/backup busybox tar cvf /backup/unifi_backup.tar /config')
+os.system('podman run --volumes-from unifi-controller --privileged --rm -v $(pwd):/backup busybox tar czvf /backup/unifi_backup.tar.gz /config')
 os.system('podman pod start unifi')
-os.system('mv /home/eingram/unifi_backup.tar /mnt/Shared/Backup/Containers/unifi-controller/unifi_backup_$(date +%m-%d).tar')
+os.system('mv /home/poduser/unifi_backup.tar.gz /mnt/Shared/Backup/Containers/unifi-controller/unifi_backup_$(date +%m-%d).tar.gz')
